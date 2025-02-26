8f4ab3864e4984fe6777c34f41494fbcf1a6c0ed
v-i-nay <vinay@softscrips.net>
2025-02-26 17:54:28 +0530

    to production

--- config/server.ts
+++ config/server.ts
@@ -1,5 +1,6 @@
 export default ({ env }) => ({
-  host: env('HOST', '::1'),
+  // host: env('HOST', '::1'),//for local development
+  host: env('HOST', '0.0.0.0'),//for server
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
